e0679197d1aeb025e6089e325c0e97341da4a9e2
Itamar Shefi <itamarshefi@gmail.com>
2023-06-12 12:45:41 +0300

    bug fixes, p-a-l query arguments

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 
 from lf_logic.generator_logic import GeneratorLogic
+from typing import Optional
+
 
 app = FastAPI()
 
 
 @app.get("/", response_class=PlainTextResponse)
-async def get_words():
+async def get_words(
+        p: Optional[str] = None, a: Optional[str] = None, l: Optional[str] = None, number_of_roots: int = 10
+):
     logic = GeneratorLogic()
-    return '\n'.join(await logic.generate())
+    return '\n'.join(await logic.generate(p=p, a=a, l=l, num_of_roots=number_of_roots))
--- lf_logic/generator_logic.py
+++ lf_logic/generator_logic.py
@@ -4,12 +4,13 @@ from __future__ import annotations
 
 import asyncio
 import random
+import re
 from typing import TYPE_CHECKING
 
 import milon.dictionaries
 from pydantic import BaseModel
 
-from lf_logic.consts import WEIGHTS
+from lf_logic import consts
 
 if TYPE_CHECKING:
     from typing import Optional
@@ -19,6 +20,23 @@ class Root(BaseModel):
     p: str
     a: str
     l: str
+
+
+class Template(BaseModel):
+    root: Root
+    template: str
+
+    @staticmethod
+    def noun_template(template):
+        return Template(root=Root(p='ק', a='ט', l='ל'), template=template)
+
+    @staticmethod
+    def verb_template(template):
+        return Template(root=Root(p='פ', a='ע', l='ל'), template=template)
+
+    @property
+    def root_regex(self):
+        return f'({self.root.p})|({self.root.a})|({self.root.l})'
 
 
 class GeneratorLogic:
@@ -37,26 +55,40 @@ class GeneratorLogic:
             word['translated'] for word in self.milon.words
         ]
 
-    async def generate(self, num_of_roots=10):
-        roots = [self._gen_random_root() for _ in range(num_of_roots)]
+    async def generate(
+            self, num_of_roots: int, p: Optional[str], a: Optional[str], l: Optional[str]
+    ):
+        roots = [self._gen_random_root(p, a, l) for _ in range(num_of_roots)]
         potential_nons = []
 
         for root in roots:
             potential_nons += random.choices([
-                weight.replace('ק', root.p).replace('ט', root.a).replace('ל', root.l)
-                for weight in WEIGHTS
+                self._populate_template(Template.noun_template(weight), root)
+                for weight in consts.WEIGHTS
             ], k=10)
+            potential_nons += [
+                self._populate_template(Template.verb_template(template), root)
+                for template in consts.VERB_TEMPLATES
+            ]
         nons = asyncio.gather(
             *[self._filter_nonword(word) for word in potential_nons]
         )
 
         return [non for non in await nons if non is not None]
 
-    def _gen_random_root(self) -> Root:
+    def _populate_template(self, template: Template, root: Root):
+        replacer_dict = {
+            template.root.p: root.p, template.root.a: root.a, template.root.l: root.l
+
+        }
+        replacer = lambda match: replacer_dict[match.string[match.start(): match.end()]]
+        return re.sub(template.root_regex, replacer, template.template)
+
+    def _gen_random_root(self, p, a, l) -> Root:
         return Root(
-            p=random.choice(self.CHARS),
-            a=random.choice(self.CHARS),
-            l=random.choice(self.CHARS),
+            p=p or random.choice(self.CHARS),
+            a=a or random.choice(self.CHARS),
+            l=l or random.choice(self.CHARS),
         )
 
     async def _filter_nonword(self, word: str) -> Optional[str]:
